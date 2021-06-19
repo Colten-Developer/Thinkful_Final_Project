4bbafc2814183afccc94cf0aa0518d1b09b8649a
Colten-Developer <ColtyG1@gmail.com>
2021-06-19 10:46:57 -0500

    database not working, US-2 100%passing US-1 19pass/2fail/21total, not seeding database and not retreiving data when data is manually inputted using DBeaver and SQL

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -32,6 +32,69 @@ function hasOnlyValidProperties(req, res, next) {
   next();
 }
 
+//validates that properties are appropriate types
+function validateProperties(req, res, next) {
+
+  let date = req.body.data.reservation_date
+  //function to see if the value is a date
+  const isDate = (value) => {
+    return (new Date(value) !== "invalid Date" && !isNaN(new Date(value)))
+  } 
+
+  let time = req.body.data.reservation_time
+  let timeArray = time.split(':')
+  let people = req.body.data.people
+
+  //the reservation date is a date
+  if(!isDate(date)){
+    return next({
+      status: 400,
+      message: `reservation_date`
+    })
+  }
+  //the inputted date object
+  let dateObject = new Date(date)
+  //todays date
+  let today = new Date()
+  //todays date object formatted
+  let todayDateObject = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDay()
+  //finds what today is in a numeric form, mon = 0, tues = 1, wed =2 
+  let numberDate = dateObject.getDay()
+  
+  //if today is tuesday were closed
+  if(numberDate == 1){
+    return next({
+      status: 400,
+      message: `closed`
+    })
+  }
+  //if the date inputted is in the past
+  if(date < todayDateObject){
+    return next({
+      status: 400,
+      message: `future`
+    })
+  }
+
+  //checks if reservations time is a time
+  if(timeArray.length != 2){
+    return next({
+      status: 400,
+      message: `reservation_time`
+    })
+  }
+
+  //checks if people is a number
+  if(typeof people != 'number'){
+    return next({
+      status: 400,
+      message: `people`
+    })
+  }
+  //if every type is valid continue down the list
+  next()
+}
+
 async function list(req, res, next) {
   const data = await service.list();
   console.log(data)
@@ -39,7 +102,6 @@ async function list(req, res, next) {
 }
 
 function create(req, res, next) {
-  console.log(req.body.data)
   service
     .create(req.body.data)
     .then((data) => res.status(201).json({ data: data[0] }))
@@ -48,5 +110,5 @@ function create(req, res, next) {
 
 module.exports = {
   list: [asyncErrorBoundary(list)],
-  create: [hasOnlyValidProperties, hasRequiredProperties, asyncErrorBoundary(create)],
+  create: [hasOnlyValidProperties, hasRequiredProperties, validateProperties, asyncErrorBoundary(create)],
 };
